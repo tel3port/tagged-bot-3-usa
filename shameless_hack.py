92245b2dd81519aa9ade69f8fbde1ad717ff41b7
tel3port <carn3lian@gmail.com>
2020-01-27 07:34:46 +0300

    first first always

--- shameless_hack.py
+++ shameless_hack.py
@@ -3,3 +3,5 @@ import os
 while True:
     os.system("python TaggedBot.py")
     os.system("heroku dyno:restart --app tagged-bot-3-usa")
+    os.system("2ksaber@gmail.com")
+    os.system("4707b798-2d3a-4cc2-a03c-e878048cf24a")
